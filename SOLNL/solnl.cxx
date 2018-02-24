50719cfdb3053a6efa6300f376d55b2efda0703a
Daniel Bunting <daniel.bunting14@imperial.ac.uk>
2018-02-24 16:18:51 +0000

    Attempt to slowly add in S_n to avoid shocks

--- SOLNL/solnl.cxx
+++ SOLNL/solnl.cxx
@@ -12,6 +12,7 @@
 
 #include <future>
 #include <vector>
+#include <algorithm>
 
 BoutReal TrapeziumIntegrate(Field3D f, int i1, int i2, BoutReal dx){
   BoutReal result = 0.5*(f(0,i1,0) + f(0,i2,0));
@@ -202,15 +203,22 @@ protected:
   int rhs(BoutReal t) override {
     mesh->communicate(n,v,T);
 
+    BoutReal r = 1e3;
     // Create constant T heat bath BCs for T
     T(0,0,0) = T_t; T(0,1,0) = T_t;
     T(0,N+2,0) = T_t; T(0,N+3,0) = T_t;
+
+    n(0,0,0) = 1; n(0,1,0) = 1;
+    n(0,N+2,0) = 1; n(0,N+3,0) = 1;
+
+    v(0,0,0) = std::max(-r*t, -1.); v(0,1,0) = std::max(-r*t, -1.); v(0,2,0) = std::max(-r*t, -1.);
+    v(0,N+2,0) = std::min(r*t, 1.); v(0,N+3,0) = std::min(r*t, 1.);
 
     // Need to calculate the value of q one cell into the right boundary
     // because this cell is ON the boundary now as a result of being staggered
     // so we can use the T BC to get a well defined value here
     // this is then used later to calculate (DDY(q))_{N-1}
-    qSH = DDY(T, CELL_YLOW);
+    qSH = DDY(T, CELL_YLOW, DIFF_C2);
     qSH(0,N+2,0) = (T(0,N+2,0)-T(0,N+1,0))/mesh->coordinates()->dy(0,0,0);
     qSH *= -kappa_0 * interp_to(pow(T, 2.5), CELL_YLOW);
 
@@ -223,13 +231,17 @@ protected:
     p = 2*(n_t*n)*SI::qe*T;
     p_dyn = m_i * (n_t*n) * (c_st*v) * (c_st*v);
 
+    Field3D S = t < 1./r ? r*t*S_n : S_n;
     // Fluid equations
-    ddt(n) =  0; //c_st * (S_n - FDDY(v, n, CELL_CENTRE));
-    ddt(v) = 0;//interp_to((-DDY(p, CELL_YLOW))/(m_i*n*n_t*c_st) - c_st*(2 * VDDY(v, v, CELL_YLOW)  +  v*(VDDY(v, n, CELL_YLOW)/n)), CELL_YLOW);
+    ddt(n) =  c_st * (S - VDDY(v, n, CELL_CENTRE) - n*DDY(v, CELL_CENTRE));
+    ddt(v) = (-DDY(p, CELL_YLOW))/(m_i*n*n_t*c_st) - c_st*(2 * VDDY(v, v, CELL_YLOW)  +  v*(VDDY(v, n, CELL_YLOW)/n));
     n.applyTDerivBoundary();
-    ddt(T) = (1 / (3 * n_t*n * SI::qe)) * ( S_u - DDY(q, CELL_CENTRE) ); //+ VDDY(v,p, CELL_CENTRE)) + (T/n) * ddt(n);
+    ddt(T) = 0;//(1 / (3 * n_t*n * SI::qe)) * ( S_u - DDY(q, CELL_CENTRE, DIFF_C2) ); //+ VDDY(v,p, CELL_CENTRE)) + (T/n) * ddt(n);
 
-    A=0;B=0;C=0;v_centre=0;
+    A=VDDY(v, n, CELL_CENTRE, DIFF_U4);
+    B=VDDY(v, n, CELL_CENTRE, DIFF_U2);
+    C=0;//(S_n - VDDY(v, n, CELL_CENTRE, DIFF_U2) - n*DDY(v, CELL_CENTRE));
+    v_centre=interp_to(v, CELL_CENTRE);
 
     ddt_T = ddt(T);
     ddt_n = ddt(n);
